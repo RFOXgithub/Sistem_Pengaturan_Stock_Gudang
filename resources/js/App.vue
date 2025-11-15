ce1efe3d6a8fb95fa97991a076901d738b3c69af
RFOXgithub <rivalmanti399@gmail.com>
2025-11-15 22:53:27 +0700

    update

---
+++ resources/js/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Halo dari App.vue!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue' 
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
